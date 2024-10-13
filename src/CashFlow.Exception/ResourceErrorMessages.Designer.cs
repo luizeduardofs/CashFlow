9a9fb67f0fbf674e1ac719937c7e875b0f2844ac
luizeduardofs <edu.dev.py@gmail.com>
2024-10-12 23:33:01 -0300

    Implement culture config to error messages

--- src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -22,7 +22,7 @@ namespace CashFlow.Exception {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ResourceErrorMessages {
+    public class ResourceErrorMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -59,47 +59,47 @@ namespace CashFlow.Exception {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to The value must be greater than zero.
         /// </summary>
-        internal static string AMOUNT_MUST_BE_GREATER_THAN_ZERO {
+        public static string AMOUNT_MUST_BE_GREATER_THAN_ZERO {
             get {
                 return ResourceManager.GetString("AMOUNT_MUST_BE_GREATER_THAN_ZERO", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Expense cannot be for the future.
         /// </summary>
-        internal static string EXPENSE_CANNOT_FOR__THE_FUTURE {
+        public static string EXPENSE_CANNOT_FOR__THE_FUTURE {
             get {
                 return ResourceManager.GetString("EXPENSE_CANNOT_FOR _THE_FUTURE", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Payment type is not valid.
         /// </summary>
-        internal static string PAYMENT_TYPE_INVALID {
+        public static string PAYMENT_TYPE_INVALID {
             get {
                 return ResourceManager.GetString("PAYMENT_TYPE_INVALID", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to The title is required.
         /// </summary>
-        internal static string TITLE_REQUIRED {
+        public static string TITLE_REQUIRED {
             get {
                 return ResourceManager.GetString("TITLE_REQUIRED", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to unknown error.
         /// </summary>
-        internal static string UNKNOWN_ERROR {
+        public static string UNKNOWN_ERROR {
             get {
                 return ResourceManager.GetString("UNKNOWN_ERROR", resourceCulture);
             }
